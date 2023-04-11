489d34ca113391dc0b03581a72f5e98eae25bad9
Larissa Benedicto <125518570+LarissaBndct@users.noreply.github.com>
2023-04-11 18:32:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[]args)
+    {
+    Console.WriteLine("Digite o número para calcular a tabuada: ");
+    Console.Write("Essa é a tabuada do ");
+    int numero = Convert.ToInt16(Console.ReadLine());
+    int tabuada = 0;
+
+    while(tabuada <= 10)
+    {
+        int resultado = tabuada * numero;
+        Console.WriteLine(tabuada+" x "+numero+" = "+resultado);
+        tabuada++;
+    }
+    
+    Console.WriteLine("Pressione qualquer tecla para encerrar");
+    Console.ReadKey();
+    }
+    
+}
